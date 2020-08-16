426a33b1f4954ddb58cc38ebf088a04ba324e25c
Karasuma1412 <bjoerninorek@gmail.com>
2020-08-16 17:20:27 +0200

    add average edits per site, plural for stats as stats page identifier

--- src/main/java/de/karasuma/discordbot/conannews/pagehandler/PageHandlerFactory.java
+++ src/main/java/de/karasuma/discordbot/conannews/pagehandler/PageHandlerFactory.java
@@ -4,20 +4,33 @@ public class PageHandlerFactory {
 
     public PageHandler getPageHandler(String searchTerm) {
         String lowerCaseSearchTerm = searchTerm.toLowerCase();
-        if (isPageType("user", "benutzer", lowerCaseSearchTerm)) {
+        if (isUserPage(lowerCaseSearchTerm)) {
             return new UserPageHandler();
-        } else if (isPageType("special", "spezial", lowerCaseSearchTerm)) {
-            return new SpecialPageHandler();
-        } else if (isPageType("statistics", "statistik", lowerCaseSearchTerm)
-                || lowerCaseSearchTerm.startsWith("stats")) {
+        } else if (isStatisticsPage(lowerCaseSearchTerm)) {
             return new StatisticsPageHandler();
+        } else if (isSpecialPage(lowerCaseSearchTerm)) {
+            return new SpecialPageHandler();
         }
 
         return new ArticlePageHandler();
     }
 
-    private boolean isPageType(String englishType, String germanType, String searchTerm) {
-        return searchTerm.startsWith(englishType) || searchTerm.startsWith(germanType);
+    private boolean isSpecialPage(String searchTerm) {
+        return searchTerm.startsWith("special") || searchTerm.startsWith("spezial");
+    }
+
+    private boolean isUserPage(String searchTerm) {
+        return searchTerm.startsWith("user") || searchTerm.startsWith("benutzer");
+    }
+
+    private boolean isStatisticsPage(String searchTerm) {
+        String searchTermWithoutSpecial = searchTerm.replace("special:", "")
+                .replace("spezial:", "");
+        return searchTermWithoutSpecial.startsWith("stats")
+                || searchTermWithoutSpecial.startsWith("statistic")
+                || searchTermWithoutSpecial.startsWith("statistik")
+                || searchTermWithoutSpecial.startsWith("statistics")
+                || searchTermWithoutSpecial.startsWith("statistiken");
     }
 
 }
