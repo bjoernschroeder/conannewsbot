dba0f5f404f38b03ea7fd61fea72c741527bab0a
Karasuma1412 <bjoerninorek@gmail.com>
2020-08-16 16:18:57 +0200

    remove unused imports

--- src/main/java/de/karasuma/discordbot/conannews/CommandWiki.java
+++ src/main/java/de/karasuma/discordbot/conannews/CommandWiki.java
@@ -3,31 +3,7 @@ package de.karasuma.discordbot.conannews;
 import de.karasuma.discordbot.commandhandling.Command;
 import de.karasuma.discordbot.conannews.pagehandler.PageHandler;
 import de.karasuma.discordbot.conannews.pagehandler.PageHandlerFactory;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-
-import javax.net.ssl.HttpsURLConnection;
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Locale;
-import java.util.Map;
 
 public class CommandWiki implements Command {
 
