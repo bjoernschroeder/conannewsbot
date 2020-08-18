e33df2cd3658c891f77232ddc1bd0730df69fd18
Karasuma1412 <bjoerninorek@gmail.com>
2020-08-18 14:36:05 +0200

    add date of origin to statistics message

--- src/main/java/de/karasuma/discordbot/conannews/pagehandler/StatisticsPageHandler.java
+++ src/main/java/de/karasuma/discordbot/conannews/pagehandler/StatisticsPageHandler.java
@@ -1,6 +1,7 @@
 package de.karasuma.discordbot.conannews.pagehandler;
 
 import de.karasuma.discordbot.conannews.CoolDownHandler;
+import de.karasuma.discordbot.conannews.util.DateUtil;
 import de.karasuma.discordbot.conannews.util.DecimalFormatUtil;
 import de.karasuma.discordbot.conannews.util.HTTPUtil;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -17,6 +18,9 @@ public class StatisticsPageHandler extends PageHandler {
     private final String STATS_REQUEST_URL =
             "https://conanwiki.org/api.php?action=query&meta=siteinfo&siprop=statistics&format=json";
     private final String STATS_WIKI_LINK = "https://conanwiki.org/wiki/Spezial:Statistik";
+    private final String FIRST_EDIT_REQUEST_URL =
+            "https://conanwiki.org/api.php?action=query" +
+                    "&prop=revisions&rvlimit=1&rvprop=timestamp&rvdir=newer&titles=Hauptseite&format=json";
 
     @Override
     public void handlePage(MessageReceivedEvent event, String searchTerm, CoolDownHandler coolDownHandler) {
@@ -27,6 +31,7 @@ public class StatisticsPageHandler extends PageHandler {
 
     private EmbedBuilder generateStatsMessage(JSONObject stats) {
         DecimalFormat decimalFormat = new DecimalFormatUtil().getDecimalFormatter();
+        DateUtil dateUtil = new DateUtil();
         return new EmbedBuilder()
                 .addField("Seiten / Artikel",
                         decimalFormat.format(stats.getInt("pages")) + " / " +
@@ -36,21 +41,36 @@ public class StatisticsPageHandler extends PageHandler {
                 .addField("Dateien", decimalFormat.format(stats.getInt("images")), true)
                 .addField("Benutzer / Aktiv", decimalFormat.format(stats.getInt("users")) + " / " +
                         decimalFormat.format(stats.getInt("activeusers")), true)
+                .addField("Gründungsdatum", dateUtil.getDateFromIso(stats.getString("originDateTimestamp"))
+                        + ", " + dateUtil.getTimeFromIso(stats.getString("originDateTimestamp")) + " Uhr", true)
                 .setDescription(STATS_WIKI_LINK)
                 .setAuthor("Statistik", STATS_WIKI_LINK, "https://conanwiki.org/favicon.png")
                 .setColor(Color.decode(SUCCESS_SEARCH_COLOR));
     }
 
     private JSONObject getStats() {
-        URL url = null;
+        URL statsRequestUrl = null;
+        URL wikiOriginDateRequestUrl = null;
         try {
-            url = new URL(STATS_REQUEST_URL);
+            statsRequestUrl = new URL(STATS_REQUEST_URL);
+            wikiOriginDateRequestUrl = new URL(FIRST_EDIT_REQUEST_URL);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        JSONObject response = new HTTPUtil().getRequest(url);
-        return response
-                .getJSONObject("query")
+        HTTPUtil httpUtil = new HTTPUtil();
+        JSONObject response = httpUtil.getRequest(statsRequestUrl);
+        JSONObject firstEdit = httpUtil.getRequest(wikiOriginDateRequestUrl);
+
+        JSONObject statistics = response.getJSONObject("query")
                 .getJSONObject("statistics");
+        String firstEditTimeStamp = firstEdit.getJSONObject("query")
+                .getJSONObject("pages")
+                .getJSONObject("1")
+                .getJSONArray("revisions")
+                .getJSONObject(0)
+                .getString("timestamp");
+
+        statistics.put("originDateTimestamp", firstEditTimeStamp);
+        return statistics;
     }
 }
